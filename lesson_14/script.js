f59d9fbb56f5da2123a31fabb2fb95b23fa9c817
Illia Lytvynov <illial@wix.com>
2020-08-18 22:07:42 +0300

    Lesson 14

---
+++ lesson_14/script.js
@@ -0,0 +1,25 @@
+import { selectCreator } from './select.js';
+import { createSelect } from './selectDinamic.js';
+
+const select1 = document.querySelector('#select1');
+selectCreator(select1);
+
+createSelect(document.querySelector('.container'), ['Usa', 'Canada', 'China']);
+createSelect(document.querySelector('.container'), [
+  'New York',
+  'Montreal',
+  'Pekin',
+  'Paris',
+  'London',
+]);
+
+// const newElement = document.createElement('h1');
+
+// newElement.textContent = "HELLO I'm new one!!";
+
+// newElement.classList.add('new-element');
+// newElement.style.backgroundColor = 'red';
+
+// const body = document.querySelector('body');
+// const container = body.querySelector('.container');
+// body.append(newElement, container);
